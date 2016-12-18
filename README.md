4775042c065329b10dd482fa2512a417449c3292
Wesley Van Pelt <wcvp@me.com>
2016-12-18 16:13:21 -0500

    Added some title stuff, now I need to change the layout method

---
+++ README.md
@@ -0,0 +1,4 @@
+All of the software that should be needed to run a Raspberry Pi in a car as a media computer.
+
+I will update this with useful information once this project is in a usable state.
+
--- pythonApp/app.py
+++ pythonApp/app.py
@@ -8,6 +8,7 @@ MEDIA_SUB_PATH = "mediacontrols/"
 
 night_mode = False
 root = Tk()
+song_info = StringVar()
 
 
 def change_vol(new_vol):
@@ -43,9 +44,16 @@ def night_mode_toggle():
     draw_everything()
 
 
+def update_song_info():
+    global song_info
+    (title, x) = Popen(["rhythmbox-client", "--print-playing-format=%tt"], stdout=PIPE).communicate()
+    (album, x) = Popen(["rhythmbox-client", "--print-playing-format=%at"], stdout=PIPE).communicate()
+    (artist, x) = Popen(["rhythmbox-client", "--print-playing-format=%ta"], stdout=PIPE).communicate()
+    song_info.set(str(title) + "\n" + str(album) + "\n" + str(artist))
+    root.after(100, update_song_info)
+
+
 def draw_everything():
-    # (stdout, stderr) = Popen(["rhythmbox-client", "--print-playing-format=%tt"], stdout=PIPE).communicate()
-    # print(stdout)
     global night_mode
     global root
 
@@ -53,20 +61,19 @@ def draw_everything():
     fgc = "#d9d9d9" if night_mode else "#2b2b2b"
     root.configure(bg=bgc)
 
-    status_frame = Frame(root)
+    status_frame = Frame(root, bg=bgc)
     status_frame.pack(anchor=CENTER)
     night_mode_image = PhotoImage(file=(NIGHT_IMAGE_PATH if night_mode else DAY_IMAGE_PATH) + "night-mode.gif").subsample(9, 9)
     power_off_image = PhotoImage(file=(NIGHT_IMAGE_PATH if night_mode else DAY_IMAGE_PATH) + "power.gif").subsample(9, 9)
-    night_mode_button = Button(status_frame, command=night_mode_toggle, image=night_mode_image)
-    power_off_button = Button(status_frame, command=power_off, image=power_off_image)
-    night_mode_button.pack(side=LEFT)
-    power_off_button.pack(side=RIGHT)
-    Label(status_frame, text="asdf").pack(side=LEFT)
+    Button(status_frame, command=night_mode_toggle, image=night_mode_image).pack(side=LEFT)
+    Button(status_frame, command=power_off, image=power_off_image).pack(side=RIGHT)
+    Label(status_frame, textvariable=song_info, bg=bgc, fg=fgc, width=50).pack(side=LEFT)
+    root.after(100, update_song_info)
 
     scale = Scale(root, command=change_vol, orient=HORIZONTAL, bg=bgc, length=800, sliderlength=75, width=50, fg=fgc)
     scale.pack(anchor=CENTER)
 
-    bottom_frame = Frame(root)
+    bottom_frame = Frame(root, bg=bgc)
     bottom_frame.pack(anchor=CENTER)
     z = 6  # Just some scaling number for the images
     media_image_path = (NIGHT_IMAGE_PATH if night_mode else DAY_IMAGE_PATH) + MEDIA_SUB_PATH
@@ -74,14 +81,10 @@ def draw_everything():
     play_image = PhotoImage(file=media_image_path + "play.gif").subsample(z, z)
     pause_image = PhotoImage(file=media_image_path + "pause.gif").subsample(z, z)
     next_image = PhotoImage(file=media_image_path + "fast-forward.gif").subsample(z, z)
-    prev_button = Button(bottom_frame, command=prev, image=prev_image)
-    play_button = Button(bottom_frame, command=play, image=play_image)
-    pause_button = Button(bottom_frame, command=pause, image=pause_image)
-    next_button = Button(bottom_frame, command=next, image=next_image)
-    prev_button.pack(side=LEFT)
-    play_button.pack(side=LEFT)
-    pause_button.pack(side=LEFT)
-    next_button.pack(side=LEFT)
+    Button(bottom_frame, command=prev, image=prev_image).pack(side=LEFT)
+    Button(bottom_frame, command=play, image=play_image).pack(side=LEFT)
+    Button(bottom_frame, command=pause, image=pause_image).pack(side=LEFT)
+    Button(bottom_frame, command=next, image=next_image).pack(side=LEFT)
 
     root.mainloop()
 
