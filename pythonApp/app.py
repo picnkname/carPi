f528a422a060c29b8ddd5cdcf63678a9b3fcd571
Wesley Van Pelt <wcvp@me.com>
2016-12-18 06:29:19 -0500

    added night mode, updated images, started some stuff with track name

--- pythonApp/app.py
+++ pythonApp/app.py
@@ -1,5 +1,13 @@
-from tkinter import *
-from subprocess import call
+from Tkinter import *
+from subprocess import call, Popen, PIPE
+
+
+DAY_IMAGE_PATH = "images-day/"
+NIGHT_IMAGE_PATH = "images-night/"
+MEDIA_SUB_PATH = "mediacontrols/"
+
+night_mode = False
+root = Tk()
 
 
 def change_vol(new_vol):
@@ -22,26 +30,60 @@ def prev():
     call(["rhythmbox-client", "--previous"])
 
 
-root = Tk()
-scale = Scale(root, command=change_vol, orient=HORIZONTAL, length=800, sliderlength=75, width=50)
-scale.pack(anchor=CENTER)
-
-bottom_frame = Frame(root)
-bottom_frame.pack(anchor=CENTER)
-
-z = 5
-prev_image = PhotoImage(file="prev.gif").subsample(z, z)
-play_image = PhotoImage(file="play.gif").subsample(z, z)
-pause_image = PhotoImage(file="pause.gif").subsample(z, z)
-next_image = PhotoImage(file="next.gif").subsample(z, z)
-prev_button = Button(bottom_frame, command=prev, image=prev_image)
-play_button = Button(bottom_frame, command=play, image=play_image)
-pause_button = Button(bottom_frame, command=pause, image=pause_image)
-next_button = Button(bottom_frame, command=next, image=next_image)
-prev_button.pack(side=LEFT)
-play_button.pack(side=LEFT)
-pause_button.pack(side=LEFT)
-next_button.pack(side=LEFT)
+def power_off():
+    call(["sudo", "shutdown", "-h", "now"])
 
 
-root.mainloop()
+def night_mode_toggle():
+    global night_mode
+    global root
+    night_mode = not night_mode
+    root.destroy()
+    root = Tk()
+    draw_everything()
+
+
+def draw_everything():
+    # (stdout, stderr) = Popen(["rhythmbox-client", "--print-playing-format=%tt"], stdout=PIPE).communicate()
+    # print(stdout)
+    global night_mode
+    global root
+
+    bgc = "#2b2b2b" if night_mode else "#d9d9d9"
+    fgc = "#d9d9d9" if night_mode else "#2b2b2b"
+    root.configure(bg=bgc)
+
+    status_frame = Frame(root)
+    status_frame.pack(anchor=CENTER)
+    night_mode_image = PhotoImage(file=(NIGHT_IMAGE_PATH if night_mode else DAY_IMAGE_PATH) + "night-mode.gif").subsample(9, 9)
+    power_off_image = PhotoImage(file=(NIGHT_IMAGE_PATH if night_mode else DAY_IMAGE_PATH) + "power.gif").subsample(9, 9)
+    night_mode_button = Button(status_frame, command=night_mode_toggle, image=night_mode_image)
+    power_off_button = Button(status_frame, command=power_off, image=power_off_image)
+    night_mode_button.pack(side=LEFT)
+    power_off_button.pack(side=RIGHT)
+    Label(status_frame, text="asdf").pack(side=LEFT)
+
+    scale = Scale(root, command=change_vol, orient=HORIZONTAL, bg=bgc, length=800, sliderlength=75, width=50, fg=fgc)
+    scale.pack(anchor=CENTER)
+
+    bottom_frame = Frame(root)
+    bottom_frame.pack(anchor=CENTER)
+    z = 6  # Just some scaling number for the images
+    media_image_path = (NIGHT_IMAGE_PATH if night_mode else DAY_IMAGE_PATH) + MEDIA_SUB_PATH
+    prev_image = PhotoImage(file=media_image_path + "rewind.gif").subsample(z, z)
+    play_image = PhotoImage(file=media_image_path + "play.gif").subsample(z, z)
+    pause_image = PhotoImage(file=media_image_path + "pause.gif").subsample(z, z)
+    next_image = PhotoImage(file=media_image_path + "fast-forward.gif").subsample(z, z)
+    prev_button = Button(bottom_frame, command=prev, image=prev_image)
+    play_button = Button(bottom_frame, command=play, image=play_image)
+    pause_button = Button(bottom_frame, command=pause, image=pause_image)
+    next_button = Button(bottom_frame, command=next, image=next_image)
+    prev_button.pack(side=LEFT)
+    play_button.pack(side=LEFT)
+    pause_button.pack(side=LEFT)
+    next_button.pack(side=LEFT)
+
+    root.mainloop()
+
+
+draw_everything()
--- setup.sh
+++ setup.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-# All of the commands to run to install everything on the pi
-#
+# All of the commands to run to install everything on the pi and my general notes until it makes its way into the readme
 # Created by Wesley Van Pelt on 2016-12-05
 # Last Modified By Wesley Van Pelt on 2016-12-05
 
 # Just the command to image the SD card on Mac OS so I dont have to keep looking it up
 # sudo dd bs=1m if=rasp-lite.img of=/dev/rdiskX
+
+# Default username and password are pi and raspberry
+# Changed screen orientation (/boot/config.txt)
+# Changed password (passwd)
+
+# Add the file "/etc/systemd/system/getty@tty1.service.d/autologin.conf" for autologin
+
+# Install htop, MATE, xinit, xinput, python3, rhythmbox, screenfetch, git, network-manager-gnome
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install htop mate-core mate-desktop-environment xinit xinput python3 rhythmbox screenfetch git network-manager-gnome
+
+# Add "startx" to ~/.bash_profile so GUI starts on boot
+
+# Put the startup.sh in ~/ and check the following:
+# "xinput --list" Then find input device, in this case it is 'BYZHYYZHY By ZHY851'
+
+# Put in "/etc/NetworkManager/NetworkManager.conf" and "/etc/NetworkManager/NetworkManager.conf"
+# Change screensaver settings, wifi/network settings, time, and add startup.sh to the startup stuff in settings
+
+# Clean up since we're done
+sudo apt-get clean
