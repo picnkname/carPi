a13a83c03b8c3cc6e81605d76b0a55c7cdfa4d36
Wesley Van Pelt <wcvp@me.com>
2017-01-03 07:31:51 -0500

    Updated configs and scripts

--- configsAndScripts/startup.sh
+++ configsAndScripts/startup.sh
@@ -4,7 +4,11 @@
 xinput set-prop 'BYZHYYZHY By ZH851' 'Evdev Axes Swap' 1
 xinput --set-prop 'BYZHYYZHY By ZH851' 'Evdev Axis Inversion' 0 1 # may need to swap last 2 ints
 
-# Start carPi GUI
+# Start carPi GUI (app.py will launch Rhythmbox)
 cd ~/carPi/pythonApp
 python app.py
+
+# nm-applet crashes on startup for some reason, so restart it after a bit
+sleep 5
+nm-applet
 
